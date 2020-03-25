2201a45ca6dfbbd82edcdbda3d7cdd1c660bdacf
0x000922 <creater3494@protonmail.com>
2020-03-25 19:20:25 +0530

    a simple socket using python using a client-server model

---
+++ python_sockets/server.py
@@ -0,0 +1,8 @@
+import socket
+seversock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+seversock.bind(('localhost',8080))
+seversock.listen(5)
+while True:
+    sock , addr = seversock.accept()
+
+
