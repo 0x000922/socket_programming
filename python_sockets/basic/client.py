ec6c2b4911ac307113ff5b5cf133dc11a38e7b1a
0x000922 <creater3494@protonmail.com>
2020-03-26 17:32:59 +0530

    various changes, chat test

--- python_sockets/basic/client.py
+++ python_sockets/basic/client.py
@@ -1,4 +1,5 @@
 import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('127.0.0.1',8080))
-
+msg = s.recv(1024)
+print(f"c2 {msg.decode("utf-8")}")
--- python_sockets/basic/server.py
+++ python_sockets/basic/server.py
@@ -4,5 +4,4 @@ seversock.bind(('localhost',8080))
 seversock.listen(5)
 while True:
     sock , addr = seversock.accept()
-
-
+    sock.send(bytes("first chat","utf-8"))
