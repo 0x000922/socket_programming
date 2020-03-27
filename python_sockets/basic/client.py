0db82a068fc0ba7cd29c3cbafd0838a44d73dc4e
0x000922 <creater3494@protonmail.com>
2020-03-27 11:45:06 +0530

    too and fro chat function implemented

--- python_sockets/basic/client.py
+++ python_sockets/basic/client.py
@@ -1,5 +1,8 @@
 import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(('127.0.0.1',8080))
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+while True:
+    msg = s.recv(1024)
+    print("from server->"+msg.decode("utf-8"))
+    msg = input("you->")
+    s.send(bytes(msg,"utf-8"))
--- python_sockets/basic/server.py
+++ python_sockets/basic/server.py
@@ -2,6 +2,11 @@ import socket
 seversock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 seversock.bind(('localhost',8080))
 seversock.listen(5)
+sock , addr = seversock.accept()
+msg = ''
 while True:
-    sock , addr = seversock.accept()
-    sock.send(bytes("first chat","utf-8"))
+    #sock.send(bytes("chat","utf-8"))
+    msg = sock.recv(1024)
+    print(repr(msg))
+    msg = input("you->")
+    sock.send(bytes(msg,"utf-8"))
